e4c9ddd682c86724f224384b23b8a790bb05b712
Miguel Palhas <mpalhas@gmail.com>
2013-11-22 17:29:47 +0000

    sha512 password

--- manifests/user.pp
+++ manifests/user.pp
@@ -3,10 +3,13 @@ define gb::user ($password) {
     ensure => present,
   }
 
+  $password_hash = str2saltedsha512($password)
+
+  # password hash can optionally begin with a $6$ to indicate libcrypt it's a SHA-512 hash
   user { $name:
     ensure     => present,
     home       => "/home/$name",
-    password   => sha1($password),
+    password   => "$6$${password_hash}",
     groups     => ['deploy', 'sudo'],
     shell      => '/bin/bash',
     managehome => true,
