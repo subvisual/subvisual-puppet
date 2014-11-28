84a6957b29eba56c398af57320e8e2652964b74e
Miguel Palhas <mpalhas@gmail.com>
2014-11-28 16:44:06 +0000

    using ensure_packages on postgresql manifest

--- puppet-module/manifests/postgresql.pp
+++ puppet-module/manifests/postgresql.pp
@@ -3,13 +3,12 @@ define gb::postgresql (
 ) {
 
   include postgresql::server
-  package { 'libpq-dev':
-    ensure => installed,
-  }
+  ensure_packages(['libpq-dev'])
 
   postgresql::server::role { $name:
     password_hash => postgresql_password($name, $password)
   }
+
   postgresql::server::db { $name:
     user     => $name,
     password => postgresql_password($name, $password)
