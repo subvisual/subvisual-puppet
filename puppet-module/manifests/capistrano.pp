904a789e4f00e60d117405f2be2215b734cf658e
Miguel Palhas <mpalhas@gmail.com>
2014-12-02 15:01:33 +0000

    Removes monit and sudoers. Adds capistrano

---
+++ puppet-module/manifests/capistrano.pp
@@ -0,0 +1,20 @@
+class gb::capistrano {
+  file { [
+      "/var/www/${name}/",
+      "/var/www/${name}/shared",
+      "/var/www/${name}/shared/sockets",
+    ]:
+    ensure => directory,
+    owner  => 'deploy',
+    group  => 'deploy',
+    mode   => 0755,
+  }
+
+  file { "/var/www/${name}/shared/.env":
+    ensure => present,
+    owner  => 'deploy',
+    group  => 'deploy',
+    mode   => 0755,
+    require => File["/var/www/${name}/shared"],
+  }
+}
