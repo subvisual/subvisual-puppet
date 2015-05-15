0b625af3b44eea63c8bcb69d35d23cf4a4b632a7
Miguel Palhas <mpalhas@gmail.com>
2015-05-15 15:29:13 +0100

    Updating base manifest

--- setup/ubuntu.pp
+++ setup/ubuntu.pp
@@ -1,21 +1,5 @@
 # base provisioning for a deploy machine
-
-# deploy user
-gb::user { 'deploy': }
-gb::public_keys { 'deploy': }
-
-# required packages
-include gb::apt_update
-include nginx
+gb::base { 'deploy' }
 
 # upstart config
 # include gb::upstart
-
-# deploy directory
-file { '/var/www':
-  ensure => directory,
-  owner  => 'deploy',
-  group  => 'deploy',
-  mode   => 0755,
-}
-
